30358b0e65ec559a0e7d0a59686cf9d8d88a7d40
ChiduAnush <chiduanush1@gmail.com>
2023-04-08 10:35:25 +0530

    start

---
+++ one.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+
+    printf("Hello world");
+    return 0;
+}
+
+// YAY!!
+// as include path wasnt working, reinstalled xcode developer tools, homebrew, and node.
+// clang compiler is working now.
