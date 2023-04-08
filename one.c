895b8fcd1329a1503d9fcb05f050f89e1a686367
ChiduAnush <chiduanush1@gmail.com>
2023-04-08 11:13:05 +0530

    forgot

--- one.c
+++ one.c
@@ -2,12 +2,19 @@
 int main()
 {
 
-    int x = 5;
-    printf("%d %d %d", ++x, ++x, ++x);
+    // int x = 5;
+    // int a = ++x + ++x;
 
-    return 0;
+    // YAY!!
+    // as include path wasnt working, reinstalled xcode developer tools, homebrew, and node.
+    // clang compiler is working now.
+
+    // variable, pre increment, pre decrement, takes the final value, for the first 2 terms.
+
+    // x+++x = x++ + x;
+    // as "post" as more precedence.
+
+    int y;
+    scanf("%d", &y);
+    (y % 400 == 0) || (y % 4 == 0 && y % 100 != 0) ? printf("YES") : printf("NO");
 }
-
-// YAY!!
-// as include path wasnt working, reinstalled xcode developer tools, homebrew, and node.
-// clang compiler is working now.
