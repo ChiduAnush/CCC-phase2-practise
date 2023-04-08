18543f0a09e2fe7b9640f3cfd4f650d22063b744
ChiduAnush <chiduanush1@gmail.com>
2023-04-08 11:31:09 +0530

    sizeof operators

--- one.c
+++ one.c
@@ -2,7 +2,14 @@
 int main()
 {
 
-    int x;
-    x = 10, 20, 30;
-    printf("%d", x);
+    printf("%ld", sizeof(long long int)); // 8
+    printf("%ld", sizeof(long double));   // 16
+
+    printf("%ld", sizeof(10));    // 4
+    printf("%ld", sizeof(10l));   // 8
+    printf("%ld", sizeof(10L));   // 8
+    printf("%ld", sizeof(10.3));  // 8  (double)
+    printf("%ld", sizeof(10.3f)); // 4 (float)
+    printf("%ld", sizeof('A'));   // 4  (by default, character gets promoted to int)
+    printf("%ld", sizeof(char));  // 1
 }
