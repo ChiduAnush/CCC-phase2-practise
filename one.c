6e4dfa57da4546c4debb3a64264cbe0a83c84a73
ChiduAnush <chiduanush1@gmail.com>
2023-04-08 11:51:55 +0530

    wat

--- one.c
+++ one.c
@@ -5,9 +5,12 @@ int main()
     // control structures:
     // write a program without semicolon
 
-    if (printf("Hello SRM"))
-    {
-        printf(" KTR");
-    }
-    printf(" RMP");
+    char c;
+    scanf("%c", &c);
+    if ((c >= 'A' && c <= 'Z') || (c >= 97 && c <= 122))
+        printf("latter");
+    else if (c >= '0' && c <= '9')
+        printf("digit");
+    else
+        printf("symbol");
 }
