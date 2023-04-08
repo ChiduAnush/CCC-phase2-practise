c1e739679b6d344baa320fb2dc58e143f9a845bd
ChiduAnush <chiduanush1@gmail.com>
2023-04-08 11:36:17 +0530

    program without semicolon in c

--- one.c
+++ one.c
@@ -2,14 +2,10 @@
 int main()
 {
 
-    printf("%ld", sizeof(long long int)); // 8
-    printf("%ld", sizeof(long double));   // 16
+    // control structures:
+    // write a program without semicolon
 
-    printf("%ld", sizeof(10));    // 4
-    printf("%ld", sizeof(10l));   // 8
-    printf("%ld", sizeof(10L));   // 8
-    printf("%ld", sizeof(10.3));  // 8  (double)
-    printf("%ld", sizeof(10.3f)); // 4 (float)
-    printf("%ld", sizeof('A'));   // 4  (by default, character gets promoted to int)
-    printf("%ld", sizeof(char));  // 1
+    if (printf("Hello SRM"))
+    {
+    }
 }
