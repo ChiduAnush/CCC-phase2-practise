25708e8ed099081c00eb35463743ad540516b720
ChiduAnush <chiduanush1@gmail.com>
2023-04-08 11:17:11 +0530

    should be 11 20 1

--- one.c
+++ one.c
@@ -2,19 +2,7 @@
 int main()
 {
 
-    // int x = 5;
-    // int a = ++x + ++x;
-
-    // YAY!!
-    // as include path wasnt working, reinstalled xcode developer tools, homebrew, and node.
-    // clang compiler is working now.
-
-    // variable, pre increment, pre decrement, takes the final value, for the first 2 terms.
-
-    // x+++x = x++ + x;
-    // as "post" as more precedence.
-
-    int y;
-    scanf("%d", &y);
-    (y % 400 == 0) || (y % 4 == 0 && y % 100 != 0) ? printf("YES") : printf("NO");
+    int x = -1, y = 20, z;
+    z = ++x || ++y;
+    printf("%d %d %d", x, y, z);
 }
