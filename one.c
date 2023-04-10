b53272e0420cb143815286f208e00f770fb2fd61
ChiduAnush <chiduanush1@gmail.com>
2023-04-10 10:50:54 +0530

    optimised for sorting?

--- one.c
+++ one.c
@@ -1,48 +1,42 @@
 #include <stdio.h>
 #include <math.h>
 
-int isprime(unsigned long n)
-{
-    if (n == 2 || n == 3)
-        return 1;
-    if (n == 1 || n % 2 == 0 || n % 3 == 0)
-        return 0;
-
-    unsigned long x = sqrt(n);
-    for (unsigned long i = 5; i <= x; i += 6)
-    {
-        if (n % i == 0 || n % (i + 2) == 0)
-            return 0;
-    }
-    return 1;
-}
-
 int main()
 {
+    int n;
+    scanf("%d", &n);
 
-    // another logic: from point 2.
-    // very very optimised one.
-    unsigned long n;
-    scanf("%ld", &n);
-
-    if (isprime(n))
+    int a[n];
+    for (int i = 0; i < n; i++)
     {
-        printf("prime");
+        scanf("%d", &a[i]);
     }
-    else
+
+    int low = 0, mid = 0, high = n - 1;
+    while (mid <= high)
     {
-        printf("not prime");
+        if (a[mid] == 0)
+        {
+            int t = a[low];
+            a[low] = a[mid];
+            a[mid] = t;
+            mid++;
+            low++;
+        }
+        else if (a[mid] == 1)
+        {
+            mid++;
+        }
+        else
+        {
+            int t = a[high];
+            a[high] = a[mid];
+            a[mid] = t;
+            high--;
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d", a[i]);
     }
 }
-
-// determine given number is prime or not?
-// exactly if 2 factors, then prime number.
-
-// 1.for any composite number, we can find out at least one factor in the range from, 2 to sqrt n.
-// So, any number u can call its prime, if it doesnt have a factor from 2 to sqrt n.
-
-// 2. Other than 2, 3, every prime can be expressed as 6*i +- 1 . (but 6*i +- 1 may or may not be prime).
-// this formula works only for 5 and greater numbers.
-
-// all prime numbers are positive. there are no negetive primes.
-//  unsigned long is faster than signed, as thers no need to verify the sign bit in unsigned.
